ded52e01efba7fbe50b9889b852d80faccbe30eb
Joe Desbonnet <jdesbonnet@gmail.com>
2013-08-11 17:59:38 +0100

    mmm... this is picking up some other device

--- src/e2-decode.c
+++ src/e2-decode.c
@@ -1,62 +1,70 @@
 #include <stdio.h>
 int main (int argc, char**argv) {
-	int v=0, pv=0;
-	int t=0, pt=0;
-	int last_symbol_t = 0;
-	int frame_mode = 0;
+	int i=0;
+	int v=0;
+	int t=0;
 	int bit_count = 0;
 	int shift_reg = 0;
-	int byte_count = 0;
+	int symbol = 0;
+	int lc=0;
 
+while(!feof(stdin)) {
+
+	// Look for frame leader
+	lc = 0;
 	while ( !feof(stdin) ) {
 		fscanf (stdin, "%d %d", &t, &v);
 
-		if (t - pt > 3000) {
-			fprintf (stdout,"\nSTART OF FRAME:\n");
-			frame_mode=1;
-			bit_count = 0;
-			shift_reg = 0;
-			byte_count = 0;
-		}
-
-		if ( frame_mode && v>=13 && v <= 17) {
-			if ( (t - last_symbol_t) > 1000) {
-				fprintf (stdout,"\n");
-			}
-			//fprintf(stdout,"1");
-			shift_reg <<= 1;
- 			shift_reg |= 0x01;
-			bit_count++;
-			if (bit_count == 8) {
-				fprintf (stdout,"[%x]",shift_reg);
-				bit_count=0;
-				shift_reg=0;
-				byte_count++;
-			}
-			last_symbol_t = t;
-		} else if (frame_mode && v >= 8 && v <= 12) {
-			if ( (t - last_symbol_t) > 1000) {
-				fprintf (stdout,"\n");
-			}
-			//fprintf(stdout,"0");
-			shift_reg <<= 1;
-			bit_count++;
-			if (bit_count == 8) {
-				fprintf (stdout,"[%x]",shift_reg);
-				bit_count=0;
-				shift_reg=0;
-				byte_count++;
-			}
-			last_symbol_t = t;
+		if (v>=6 && v <=8) {
+			lc++;
 		} else {
-			if (frame_mode) {
-				fprintf (stdout,"***\n***\n");
+			if (lc > 3000) {
+				break;
+			} else {
+				lc = 0;
 			}
-			frame_mode = 0;
 		}
 
-		pv = v;
-		pt = t;
+		i++;
+		if (i % 10000 == 0) {
+			fprintf (stdout,"*");
+			fflush (stdout);
+		}
+	}
+
+	fprintf (stdout,"\nSTART OF FRAME: lc=%d ",lc);
+	bit_count = 0;
+	shift_reg = 0;
+
+
+	while ( !feof(stdin) ) {
+		fscanf (stdin, "%d %d", &t, &v);
+		//fprintf (stdout, "v=%d ",v);
+		if ( v>=13 && v <= 17) {
+			symbol=1;
+		} else if ( v >= 9 && v <= 12) {
+			symbol=0;
+		} else {
+			symbol=-1;
+		}
+
+		if (symbol != -1) {
+			shift_reg <<= 1;
+ 			shift_reg |= symbol;
+			bit_count++;
+			if (bit_count % 8 == 0) {
+				fprintf (stdout,"%x ",shift_reg & 0xff);
+				if (bit_count > 1000*8) {
+					fprintf (stdout," END OF FRAME.\n");
+					break;
+				}
+			}
+		} else {
+			fprintf (stdout,"*%d* ",v);
+		} 
+		
+	}
+
 	}
 
 }
